040ab67f219732df34ab7ce7dd1af1dff91ba228
unknown <victordinami@gmail.com>
2018-12-05 21:53:43 -0200

    Implementos Portifolio~

--- website/views.py
+++ website/views.py
@@ -3,10 +3,5 @@ from django.http import HttpResponse
 import datetime
 
 def home(request):
-    #now = datetime.datetime.now()
-    #html = "<html><body>It is now %s.</body></html>" % now
-    #return HttpResponse(html)
     data= {}
-    data['transacoes']=['t1', 't2', 't3']
-    data['now']=datetime.datetime.now()
     return render(request, 'website/home.html', data)
