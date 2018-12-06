66824af902070341da0761019bf7889144b161df
VictorLM <victordinami@gmail.com>
2018-12-06 16:00:39 -0200

    correção Gunicorn

--- gunicorn/gunicorn-config.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-bind = 'unix:/tmp/gunicorn.sock'
-workers = multiprocessing.cpu_count() * 2 + 1
-reload = True
-daemon = True
-accesslog = './access.log'
-errorlog = './error.log'
