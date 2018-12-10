2d7662b30f8abac9b7e956f5fa4902aff576bb6f
VictorLM <victordinami@gmail.com>
2018-12-10 16:11:50 -0200

    Correcao STATIC_FILES

--- jeto_pro/settings.py
+++ jeto_pro/settings.py
@@ -110,8 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
