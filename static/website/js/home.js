c2e1da02f402042108c294132df1fd058c78cfa4
VictorLM <victordinami@gmail.com>
2018-12-10 08:46:37 -0200

    Alteracoes home

--- static/website/js/home.js
+++ static/website/js/home.js
@@ -1,5 +1,5 @@
 var typed2 = new Typed('.h1-home-header', {
-    strings: ['<b>WORK HARDER</b>','<b>WORK SMARTER</b>','<b>WORK SMARTER =)</b>','<b>WORK SMARTER</b>'],
+    strings: ['<b>WORK HARDER</b>','<b>WORK SMARTER</b>'],
     startDelay: 1000,
     typeSpeed: 100,
     backSpeed: 50,
